ab3614b95144d5bb8910f1f233342562d4ea5c44
ShinYoungHo <dydh1230219@gmail.com>
2021-05-12 23:07:54 +0900

    [JAWS-8][Add] repeated router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from 'express';
-import app from './loaders/express';
+import express from 'express';
+import mongoose from './loaders/mongoose';
+import route from './API/routes/index';
+const app = express();
+
+// app.use()
+require('./loaders/express').default({ app });
+
+// mongoose
+mongoose().then(()=>{
+  console.log('database connected');
+})
+
+// route
+app.use('/users', route.users)
 
 app.get('/', (req: Request, res: Response) => {
   res.send({ message: 'hello world!' });
