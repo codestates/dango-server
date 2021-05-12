199b150a63793e1a3d55c8390c6c25f7bf93f7ba
ShinYoungHo <dydh1230219@gmail.com>
2021-05-12 23:06:37 +0900

    [JAWS-8][Fix] change location of express settings

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,14 +1,20 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import cookieparser from 'cookie-parser';
-import mylogger from '../middleware/mylogger';
+import mylogger from '../API/middleware/mylogger';
+import config from '../config/index';
 
-const app: Application = express();
+export default ({ app }: { app: Application }) => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieparser());
+  app.use(
+    cors({
+      origin: config.clientURL,
+      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    }),
+  );
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieparser());
-app.use(mylogger);
-
-
-export default app;
+  app.use(mylogger);
+};
