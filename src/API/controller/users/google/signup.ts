54be7245404c81765854b666eee1f4a6076498f0
ShinYoungHo <dydh1230219@gmail.com>
2021-05-14 22:59:00 +0900

    [JAWS-12][Add] google signup controller

---
+++ src/API/controller/users/google/signup.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import UserModel from '../../../../models/user';
+import GoogleAuth from '../../../../service/google';
+import config from '../../../../config/index';
+
+export default async (req: Request, res: Response) => {
+  const IdToken: string = req.headers.authorization?.split(' ')[1]!;
+  const nickname: string = req.body.nickname;
+
+  try {
+    const dbNickname = await UserModel.findOne({ nickname: nickname });
+    if (dbNickname) {
+      res.status(409).send({ message: '이미 존재하는 닉네임입니다.' });
+    } else {
+      const userData = await GoogleAuth.getGoogleProfile(IdToken);
+      if (userData) {
+        const dbData = await UserModel.findOne({ 'socialData.id': userData.sub });
+        if (dbData) {
+          res.status(409).send({ message: '이미 회원가입이 된 유저입니다.' });
+        } else {
+          const userInfo = {
+            nickname,
+            socialData: {
+              id: userData.sub,
+              type: 'google',
+              name: userData.name,
+              email: userData.email,
+              image: userData.picture || config.defaultImage,
+            },
+          };
+          const newUser = new UserModel(userInfo);
+          await newUser.save();
+          res.status(200).send({ message: '회원가입에 성공했습니다.', nickname, userInfo });
+        }
+      } else {
+        res.status(401).send({ message: '유효하지 않은 토큰입니다.' });
+      }
+    }
+  } catch (err) {
+    res.status(500).send({ message: '서버오류로 응답에 실패했습니다.' });
+  }
+};
