276bf4151d48320e5b7d0189d73d7563d30db27a
ShinYoungHo <dydh1230219@gmail.com>
2021-05-18 02:16:34 +0900

    [JAWS-30][Add] talentMapSort

--- src/API/controller/talents/map.ts
+++ src/API/controller/talents/map.ts
@@ -6,9 +6,10 @@ export default async (req: Request, res: Response) => {
   const [S, N]: number[] = req.body.width;
   const [lat, lon]: number[] = req.body.location; // 쿼리 보낼땐 반대로
   const category: string = req.body.category;
-
+  const sort: 'price' | 'ratings' | 'review' = req.body.sort;
+  const finalSort = sort === 'price' ? 'price' : sort === 'ratings' ? '-ratings.0' : '-ratings.1';
   const width = solveMapWidth([S, N]);
-
+  console.log(width);
   try {
     const previewsArr = await TalentModel.find({
       location: {
@@ -22,12 +23,13 @@ export default async (req: Request, res: Response) => {
       .find({ category })
       .populate('userInfo', 'nickname')
       .select('location ratings category title price')
+      .sort(`${finalSort}`)
       .lean();
-
+    console.log(previewsArr);
     res.json({ result: previewsArr, message: '주변 데이터 불러오기에 성공했습니다.' });
   } catch (err) {
     res.json({ message: '서버 응답에 실패했습니다.' });
   }
 };
 
-// 123,50 :::
+// sort : price, ratings, review
