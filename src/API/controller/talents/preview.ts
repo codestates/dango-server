2ffe9fa148df597b886b0ff243a6617f6d9585b9
ShinYoungHo <dydh1230219@gmail.com>
2021-05-18 02:47:45 +0900

    [JAWS-30][Add] mapPreview

---
+++ src/API/controller/talents/preview.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import TalentModel from '../../../models/talents';
+
+export default async (req: Request, res: Response) => {
+  const oid: string = req.params.talentId;
+  try {
+    const result = await TalentModel.findOne({ _id: oid }).select('-__v -images -reviews -userInfo').lean();
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).json({ message: '재능 정보를 찾을 수 없습니다.' });
+    }
+  } catch (err) {
+    if (err.name === 'CastError') {
+      res.status(404).json({ message: '유효하지 않은 id 입니다' });
+    } else {
+      res.status(500).json({ message: '서버응답에 실패했습니다' });
+    }
+  }
+};
+// 위치, 카테고리, 닉네임,  별점 평균, 가격, 글 제목, 글 내용, 지역
--- src/API/routes/talents.ts
+++ src/API/routes/talents.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import getTalentsBylocation from '../controller/talents/map';
 import createTalent from '../controller/talents/create';
-
+import getPreview from '../controller/talents/preview';
+  
 const router = Router();
 
 router.get('/map', getTalentsBylocation);
 router.post('/create', createTalent);
+router.get('/preview/:talentId',getPreview );
+
 export default router;
 
 /*
