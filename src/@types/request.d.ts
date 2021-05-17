60445c4ec20e47df3e6ed5900652e411a1fe5c26
ShinYoungHo <dydh1230219@gmail.com>
2021-05-17 21:46:20 +0900

    [JAWS-28][Add] talent saving query

---
+++ src/@types/request.d.ts
@@ -0,0 +1,10 @@
+export interface CreateTalent {
+  _id?:string;
+  title:string;
+  location:number[];
+  roadName:string;
+  category:string;
+  price:number;
+  detail:string;
+  images:string[];
+}
--- src/API/controller/talents/create.ts
+++ src/API/controller/talents/create.ts
@@ -1,18 +1,34 @@
+import { CreateTalent } from './../../../@types/request.d';
 import { Request, Response } from 'express';
 import TalentModel from '../../../models/talents';
 
 export default async (req: Request, res: Response) => {
+  const reqData: CreateTalent = req.body;
   try {
+    const talent = {
+      ...reqData,
+      userInfo: reqData._id,
+    };
+    delete talent._id;
+    const talentDoc = new TalentModel(talent);
+    talentDoc.save((err, talent) => {
+      console.log(err);
+      console.log(123);
+      console.log(talent);
+      res.send(talent);
+    });
   } catch (err) {
+    res.json({ message: '서버 응답에 실패했습니다.' });
   }
 };
 
 /**
- * title
- * location
- * city
- * category
- * price
- * detail
- * images:[]
+ _id
+ title
+ location
+ city
+ category
+ price
+ detail
+ images:[]
  */
