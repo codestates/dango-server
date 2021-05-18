25a095a0f294ec5d998af32b926ee244d56400d6
ShinYoungHo <dydh1230219@gmail.com>
2021-05-18 14:01:04 +0900

    [JAWS-32][Feat] filter by categorys array

--- src/API/controller/talents/map.ts
+++ src/API/controller/talents/map.ts
@@ -5,9 +5,9 @@ import solveMapWidth from '../../../service/coordinates';
 export default async (req: Request, res: Response) => {
   const [S, N]: number[] = req.body.width;
   const [lat, lon]: number[] = req.body.location; // 쿼리 보낼땐 반대로
-  const category: string = req.body.category;
+  const categorys: string[] = req.body.category;
   const sort: 'price' | 'ratings' | 'review' | undefined = req.body.sort;
-  const finalSort = sort === 'price' ? 'price' : sort === 'ratings' ? '-ratings.0' : '-ratings.1' || ''
+  const finalSort = sort === 'price' ? 'price' : sort === 'ratings' ? '-ratings.0' : '-ratings.1' || '';
   const width = solveMapWidth([S, N]);
   try {
     const previewsArr = await TalentModel.find({
@@ -19,13 +19,18 @@ export default async (req: Request, res: Response) => {
         },
       },
     })
-      .find({ category })
+      .find({
+        $or: categorys.sort().map((category) => {
+          return { category };
+        }),
+      })
       .populate('userInfo', 'nickname')
       .select('location ratings category title price')
       .sort(`${finalSort}`)
       .lean();
     res.json({ result: previewsArr, message: '주변 데이터 불러오기에 성공했습니다.' });
   } catch (err) {
+    console.log(err);
     res.json({ message: '서버 응답에 실패했습니다.' });
   }
 };
