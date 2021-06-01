3bfd1f8573c8ba2453a263b87de05571638038dd
ShinYoungHo <dydh1230219@gmail.com>
2021-06-01 13:17:58 +0900

    [JAWS-86][Fix] $addToSet query for build

--- src/loaders/mongoose.ts
+++ src/loaders/mongoose.ts
@@ -3,7 +3,6 @@ import { Db } from 'mongodb';
 import config from '../config/key';
 import UserModel from '../models/user';
 import TalentModel from '../models/talents';
-import { fakeTalent, fakeUser } from '../common/fakeData/models';
 
 export default async (): Promise<Db> => {
   const connection = await mongoose.connect(config.databaseURL!, {
@@ -13,8 +12,8 @@ export default async (): Promise<Db> => {
     useFindAndModify: false,
   });
 
-  const userdoc = new UserModel(fakeUser);
-  const talentdoc = new TalentModel(fakeTalent);
+  // const userdoc = new UserModel(fakeUser);
+  // const talentdoc = new TalentModel(fakeTalent);
 
   // 테스트 데이터 생성시 아래 주석을 풀어주세요.
   // 이제 create 요청으로 테스트데이터 만들 수 있습니다.
