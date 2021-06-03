9198a66a0b792f3607de8553e7ce76ea51aea180
ShinYoungHo <dydh1230219@gmail.com>
2021-06-04 02:57:47 +0900

    [JAWS-105][Add] authCheck middleware

--- src/API/middleware/auth.ts
+++ src/API/middleware/auth.ts
@@ -1,7 +1,33 @@
-/**
- * 토큰 타입 확인 ::::: length?
- * 토큰으로 유저정보 받아오고
- * 유저정보 디비에 있는지 확인한 후
- * 있으면 next()
- * 없으면 res.status(401).json({message:"유효하지 않은 유저입니다."})
- */
+import { Request, Response, NextFunction } from 'express';
+import UserModel from '../../models/user';
+import GoogleAuth from '../../service/google';
+import KakaoAuth from '../../service/kakao';
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  const token: string = req.headers.authorization?.split(' ')[1]!;
+  if (token.length > 100) {
+    const data = await GoogleAuth.getGoogleProfile(token);
+    if (data) {
+      const isInDB = await UserModel.findOne({ 'socialData.id': data.sub });
+      if (isInDB) {
+        next();
+      } else {
+        res.status(401).json({ message: '등록된 회원이 아닙니다.' });
+      }
+    } else {
+      res.status(404).json({ message: '유효하지 않은 토큰입니다.' });
+    }
+  } else {
+    const data = await KakaoAuth.getUserInfo(token);
+    if (data) {
+      const isInDB = await UserModel.findOne({ 'socialData.id': data.id });
+      if (isInDB) {
+        next();
+      } else {
+        res.status(401).json({ message: '등록된 회원이 아닙니다.' });
+      }
+    } else {
+      res.status(404).json({ message: '유효하지 않은 토큰입니다.' });
+    }
+  }
+};
