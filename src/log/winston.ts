0c8e216ed5e3fecddf7e7dca7f1d4b70c99bd150
ShinYoungHo <dydh1230219@gmail.com>
2021-06-06 20:55:49 +0900

    [JAWS-113][Fix] change logfile dir

--- src/log/winston.ts
+++ src/log/winston.ts
@@ -1,10 +1,16 @@
 import winston from 'winston';
+import fs from 'fs';
+const env = process.env.NODE_ENV;
+
+if (!fs.existsSync('logs')) {
+  fs.mkdirSync('logs');
+}
 
 const logger = winston.createLogger({
   level: 'debug',
   transports: [
     new winston.transports.File({
-      filename: 'system.log',
+      filename: './logs/system.log',
       format: winston.format.printf(
         info => `${new Date().toLocaleTimeString()} [${info.level.toUpperCase()}] - ${info.message}`)
     }),
